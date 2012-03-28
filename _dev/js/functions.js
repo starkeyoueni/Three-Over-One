b39062a97f7632bf85e85f50793494d87e998eb5
David Parry <david.parry@suranyami.com>
2012-03-28 11:19:45 +1100

    Downloaded theme from shopify

--- _dev/js/functions.js
+++ _dev/js/functions.js
@@ -1,81 +1,61 @@
-$(function(){
-		
-		
-	//BINDINGS
-	$(".product-row a").bind("click", scrollToPlace);
-		   
-	
+$(function() {
+  //BINDINGS
+  $(".product-row a").bind("click", scrollToPlace);
 });//
 
-
-
-
-
-
-function scrollToPlace(e){
-
-	e.preventDefault();
+function scrollToPlace(e) {
+  e.preventDefault();
 	
-	$(".product-row a").removeClass("showing");
-	$(this).addClass("showing");
+  $(".product-row a").removeClass("showing");
+  $(this).addClass("showing");
 	
-	var productDetail = $(this).parent().parent();
+  var productDetail = $(this).parent().parent();
 	
-	//CURRENT CONTAINER
-	productDetail.container = productDetail.find(".product-detail");
-	productDetail.container.inner = productDetail.container.find(".inner");
-	productDetail.height = productDetail.container.inner.height();
-	productDetail.position = productDetail.index();
+  //CURRENT CONTAINER
+  productDetail.container = productDetail.find(".product-detail");
+  productDetail.container.inner = productDetail.container.find(".inner");
+  productDetail.height = productDetail.container.inner.height();
+  productDetail.position = productDetail.index();
 	
-	//OPEN CONTAINER
-	productDetail.Open = productDetail.siblings().filter(".open");
-	productDetail.Open.container = productDetail.Open.find(".product-detail");
-	productDetail.Open.container.inner = productDetail.Open.container.find(".inner");
-	productDetail.Open.height = productDetail.Open.container.inner.height();
-	productDetail.Open.position = productDetail.Open.index();
+  //OPEN CONTAINER
+  productDetail.Open = productDetail.siblings().filter(".open");
+  productDetail.Open.container = productDetail.Open.find(".product-detail");
+  productDetail.Open.container.inner = productDetail.Open.container.find(".inner");
+  productDetail.Open.height = productDetail.Open.container.inner.height();
+  productDetail.Open.position = productDetail.Open.index();
 	
-	//SCROLL TO VALUE
-	productDetail.scrollto = function(){
-		
-		if(productDetail.Open.isAfter()){	
-		//IF NO OPEN OR IF OPEN AREA IS BELOW TO-BO OPENED AREA
-			return productDetail.offset().top - $("header").height() - 20;
-		}else{
-		//IF OPEN AREA IS BEFORE TO-BE OPENED AREA - TAKE AWAY THE HEIGHT OF THE INNER
-		//REVISION: TAKE THE HEIGHT OF THE OPEN CONTAINER
-			return productDetail.offset().top - productDetail.Open.height - $("header").height() - 20;
-		}
-		
-	}
+  //SCROLL TO VALUE
+  productDetail.scrollto = function() {
+    if (productDetail.Open.isAfter()) {	
+      //IF NO OPEN OR IF OPEN AREA IS BELOW TO-BO OPENED AREA
+      return productDetail.offset().top - $("header").height() - 20;
+    } else {
+      //IF OPEN AREA IS BEFORE TO-BE OPENED AREA - TAKE AWAY THE HEIGHT OF THE INNER
+      //REVISION: TAKE THE HEIGHT OF THE OPEN CONTAINER
+      return productDetail.offset().top - productDetail.Open.height - $("header").height() - 20;
+    }
+  }
 	
-	//CONFIG
-	productDetail.config = {
-		
-		openSpeed: 700,
-		scrollSpeed: 700,
-		
-	}
+  //CONFIG
+  productDetail.config = {
+    openSpeed: 700,
+    scrollSpeed: 700,
+  }
 	
-	//FIND ORDER
-	productDetail.Open.isAfter = function(){
-		
-		if (productDetail.Open.position === -1 || productDetail.Open.position > productDetail.position){
-			return true;
-		}else{
-			return false;
-		}
-		
-	}
+  //FIND ORDER
+  productDetail.Open.isAfter = function() {
+    if (productDetail.Open.position === -1 || productDetail.Open.position > productDetail.position) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 	
-	
-// // // // // 	ANIMATIONS:
-	
-	//OPEN
-	productDetail.addClass("open").container.stop().animate({"height" : productDetail.height}, productDetail.config.openSpeed);
-	//CLOSE
-	productDetail.Open.removeClass("open").container.stop().animate({"height" : "0px"}, productDetail.config.openSpeed);
-	//SCROLL
-	$("body").stop().animate({scrollTop: productDetail.scrollto()}, productDetail.config.scrollSpeed);
-							 
-	
+  // // // // // 	ANIMATIONS:
+  //OPEN
+  productDetail.addClass("open").container.stop().animate({"height" : productDetail.height}, productDetail.config.openSpeed);
+  //CLOSE
+  productDetail.Open.removeClass("open").container.stop().animate({"height" : "0px"}, productDetail.config.openSpeed);
+  //SCROLL
+  $("body").stop().animate({scrollTop: productDetail.scrollto()}, productDetail.config.scrollSpeed);
 }
