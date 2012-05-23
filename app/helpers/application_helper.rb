2b017728fd4bbaacec895395a4bf1e0160926c04
wolfazor <steven.mileski@youeni.com>
2012-05-23 14:19:34 +1000

    Translate liquid to haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@ module ApplicationHelper
   def category_link(category)
     link_to category.name, collection_path(:params => {:category_key => category.key})
   end
-  # 
-  # - if link.active
-  #   class="active"
-  #   - endif
-  #   >#{link.title | link_to: link.url}
+  def cart_count
+    count = cart.item_count
+    items = 'item'
+    items.pluralize if count > 1 || count == 0
+    "#{count} #{items}"
+  end
   
+  def cart_total
+    "$#{cart.total_price}"
+  end
 end
