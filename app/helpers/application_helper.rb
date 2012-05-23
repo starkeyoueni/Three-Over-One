4eda19c2d3c3eab50c81c4420016e4a5cdcd61ee
David Parry <david.parry@suranyami.com>
2012-05-23 13:28:05 +1000

    Deleted useless comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,4 @@ module ApplicationHelper
   def category_link(category)
     link_to category.name, collection_path(:params => {:category_key => category.key})
   end
-  # 
-  # - if link.active
-  #   class="active"
-  #   - endif
-  #   >#{link.title | link_to: link.url}
-  
 end
