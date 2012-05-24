5a93353c46bd0bf855b5b4651df702af158806b3
David Parry <david.parry@suranyami.com>
2012-05-24 14:17:14 +1000

    Added serialization methods.

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,16 @@
 class Option
   include Mongoid::Document
   field :name,          :type => String
+
+  def serialize(object)
+    {
+      "name"   => object.name
+    }
+  end
+
+  def self.from_shopify(shopify_option)
+    option = Option.new
+    option.name =  shopify_option.name
+    option
+  end
 end
