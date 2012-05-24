97bcad6257f12ef462f3b348de4fc845029738a8
David Parry <david.parry@suranyami.com>
2012-05-24 22:50:24 +1000

    Added arrows.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,12 +6,14 @@
 //= require slides.jquery
 
 $(document).ready(function() {
-	$('#slides').slides({
+  var options = {
 		preload: true,
 		generateNextPrev: false,
     pagination: false,
     generatePagination: false,
     autoHeight: true,
     autoHeightSpeed: 350
-	});
+	};
+  console.log(options);
+	$('#slides').slides(options);
 });
