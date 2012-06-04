bf6c6392a503dd04e540bc9451e1900ca5fe301e
David Parry <david.parry@suranyami.com>
2012-06-04 17:30:09 +1000

    Got click-scrolling working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require jquery.scrollTo
 //= require jquery.easing
+//= require jquery.scrollTo
 //= require jquery.lightbox-min
 //= require pages
 //= require products
